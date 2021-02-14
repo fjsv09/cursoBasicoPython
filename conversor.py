b158f697f1b27a30ae548636ef59c26dba9a355e
fjsv09 <fjsv.cjoseph@gmail.com>
2021-02-14 13:08:40 -0500

    varios países en mi conversor de mmonedas

--- conversor.py
+++ conversor.py
@@ -1,8 +1,37 @@
-pesos = input("¿Cuántoas soles tienes?: ")
-pesos = float(pesos)
-valor_dolar = 3.6
-dolares = pesos / valor_dolar
-dolares = round(dolares, 2)
-dolares = str(dolares)
+menu = """
+Bienvenido al conversor de monedas 😎
 
-print("Tienes $" + dolares + " dolares")
+1 - Pesos colombianos
+2 - Pesos argentinos
+3 - Pesos mexicanos 
+
+Elige una opción: """
+
+opcion = input(menu)
+
+if opcion == "1":
+    pesos = input("¿Cuántos pesos colombianos tienes?: ")
+    pesos = float(pesos)
+    valor_dolar = 3875
+    dolares = pesos / valor_dolar
+    dolares = round(dolares, 2)
+    dolares = str(dolares)
+    print("Tienes $" + dolares + " dolares")
+elif opcion == "2":
+    pesos = input("¿Cuántos pesos argentinos tienes?: ")
+    pesos = float(pesos)
+    valor_dolar = 65
+    dolares = pesos / valor_dolar
+    dolares = round(dolares, 2)
+    dolares = str(dolares)
+    print("Tienes $" + dolares + " dolares")
+elif opcion == "3":
+    pesos = input("¿Cuántos pesos mexicanos tienes?: ")
+    pesos = float(pesos)
+    valor_dolar = 25
+    dolares = pesos / valor_dolar
+    dolares = round(dolares, 2)
+    dolares = str(dolares)
+    print("Tienes $" + dolares + " dolares")
+else:
+    print("Ingresa una opción correcta por favor!!!")
